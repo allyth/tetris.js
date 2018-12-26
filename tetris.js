582f07ed32130efff724c9ecee461145092a2065
Ally <aly.theo@gmail.com>
2018-12-26 17:14:52 -0600

    Added grid

--- tetris.js
+++ tetris.js
@@ -1,13 +1,25 @@
 // @ts-check
-
-const boardWidth = 10;
-const boardHeight = 20;
-const board = [[]];
-
-board[0][9] = null;
-
 const boardCanvas = /** @type {HTMLCanvasElement} */ (document.getElementById('board'));
 const boardCtx = boardCanvas.getContext('2d');
 
-boardCtx.fillStyle = 'green';
-boardCtx.fillRect(10, 10, 150, 100);
+const boardColumns = 10;
+const boardRows = 20;
+const board = [[]];
+const columnWidth = boardCanvas.width / boardColumns;
+const columnHeight = boardCanvas.height / boardRows;
+board[0][9] = null;
+
+
+for (let i = 1; i <= boardColumns - 1; i++) {
+    boardCtx.beginPath();
+    boardCtx.moveTo(columnWidth * i, 0);
+    boardCtx.lineTo(columnWidth * i, boardCanvas.height);
+    boardCtx.stroke();
+}
+
+for (let i = 1; i <= boardRows - 1; i++) {
+    boardCtx.beginPath();
+    boardCtx.moveTo(0, columnHeight * i);
+    boardCtx.lineTo(boardCanvas.height, columnHeight * i);
+    boardCtx.stroke();
+}
